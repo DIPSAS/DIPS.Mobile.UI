1420be5ca7d9f3f56e32a963691277c3a66a142b
Håvard Moås <hmo@dips.no>
2023-02-03 10:45:07 +0100

    added unit tests

---
+++ src/library/tests/DIPS.Mobile.UI.UnitTests/Converters/ValueConverters/AdditionConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DIPS.Mobile.UI.Converters.ValueConverters;
+using DIPS.Mobile.UI.UnitTests.TestHelpers;
+using FluentAssertions;
+using Xamarin.Forms.Xaml;
+using Xunit;
+
+namespace DIPS.Mobile.UI.UnitTests.Converters.ValueConverters
+{
+    public class AdditionConverterTests
+    {
+        private readonly AdditionConverter m_additionConverter = new AdditionConverter();
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(50, 100, 150)]
+        [InlineData(50000, 100000, 150000)]
+        [InlineData(-5, -5, -10)]
+        [InlineData(0, -5, -5)]
+        public void Convert_AddTermWithAddend_ShouldBeTheCorrectSum(double term, double addend, double expectedSum)
+        {
+            m_additionConverter.Addend = addend;
+            var actualSum = m_additionConverter.Convert<double>(term);
+            actualSum.Should().Be(expectedSum);
+        }
+
+        [Fact]
+        public void ConvertBack_ShouldThrowNotImplementedException()
+        {
+            Action act = () => m_additionConverter.ConvertBack<double>(0);
+            act.Should().Throw<NotImplementedException>();
+        }
+
+        [Fact]
+        public void Convert_ValueIsNull_XamlParseExceptionThrown()
+        {
+            Action act = () => m_additionConverter.Convert(null, null, null, null);
+
+            act.Should().Throw<XamlParseException>();
+        }
+
+        [Fact]
+        public void Convert_AddendIsNull_XamlParseExceptionThrown()
+        {
+            Action act = () => m_additionConverter.Convert(1.0, null, null, null);
+
+            act.Should().Throw<XamlParseException>();
+        }
+    }
+
+}
---
+++ src/library/tests/DIPS.Mobile.UI.UnitTests/Converters/ValueConverters/BoolToObjectConverterTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using DIPS.Mobile.UI.Converters.ValueConverters;
+using FluentAssertions;
+using Xamarin.Forms.Xaml;
+using Xunit;
+
+namespace DIPS.Mobile.UI.UnitTests.Converters.ValueConverters
+{
+    public class BoolToObjectConverterTests
+    {
+        private readonly BoolToObjectConverter m_boolToObjectConverter;
+
+        public BoolToObjectConverterTests()
+        {
+            m_boolToObjectConverter = new BoolToObjectConverter();
+        }
+
+        [Theory]
+        [InlineData(10, 20)]
+        [InlineData("True string", "False string")]
+        [InlineData(0.01, 0.02)]
+        [InlineData(0.01f, 0.02f)]
+        [InlineData(10, 20, true)]
+        [InlineData("True string", "False string", true)]
+        [InlineData(0.01, 0.02, true)]
+        [InlineData(0.01f, 0.02f, true)]
+
+        public void Convert_WhenValueIsTrue_CorrectOutput(object trueObject, object falseObject, bool inverted = false)
+        {
+            m_boolToObjectConverter.TrueObject = trueObject;
+            m_boolToObjectConverter.FalseObject = falseObject;
+            m_boolToObjectConverter.Inverted = inverted;
+
+            var result = m_boolToObjectConverter.Convert(true, null, null, null);
+
+            result.Should().Be(inverted ? falseObject : trueObject);
+        }
+
+        [Theory]
+        [InlineData(10, 20)]
+        [InlineData("True string", "False string")]
+        [InlineData(0.01, 0.02)]
+        [InlineData(0.01f, 0.02f)]
+        [InlineData(10, 20, true)]
+        [InlineData("True string", "False string", true)]
+        [InlineData(0.01, 0.02, true)]
+        [InlineData(0.01f, 0.02f, true)]
+
+        public void Convert_WhenValueIsFalse_CorrectOutput(object trueObject, object falseObject, bool inverted = false)
+        {
+            m_boolToObjectConverter.TrueObject = trueObject;
+            m_boolToObjectConverter.FalseObject = falseObject;
+            m_boolToObjectConverter.Inverted = inverted;
+
+            var result = m_boolToObjectConverter.Convert(false, null, null, null);
+
+            result.Should().Be(inverted ? trueObject : falseObject);
+        }
+
+        [Fact]
+        public void Convert_TrueFalseObjectsAreDifferent_ThrowsXamlParseException()
+        {
+            m_boolToObjectConverter.TrueObject = "2.0";
+            m_boolToObjectConverter.FalseObject = 2.0;
+
+            Action act = () => m_boolToObjectConverter.Convert(false, null, null, null);
+
+            act.Should().Throw<XamlParseException>();
+        }
+
+        [Fact]
+        public void Convert_InputIsNull_XamlParseExceptionThrown()
+        {
+            Action act = () => m_boolToObjectConverter.Convert(null, null, null, null);
+
+            act.Should().Throw<XamlParseException>();
+        }
+
+        [Fact]
+        public void Convert_TrueObjectIsNull_XamlParseExceptionThrown()
+        {
+            m_boolToObjectConverter.TrueObject = null;
+            m_boolToObjectConverter.FalseObject = "Something";
+            Action act = () => m_boolToObjectConverter.Convert(true, null, null, null);
+
+            act.Should().Throw<XamlParseException>();
+        }
+        
+        [Fact]
+        public void Convert_FalseObjectIsNull_XamlParseExceptionThrown()
+        {
+            m_boolToObjectConverter.FalseObject = null;
+            m_boolToObjectConverter.TrueObject = "Something";
+            Action act = () => m_boolToObjectConverter.Convert(true, null, null, null);
+
+            act.Should().Throw<XamlParseException>();
+        }
+    }
+}
