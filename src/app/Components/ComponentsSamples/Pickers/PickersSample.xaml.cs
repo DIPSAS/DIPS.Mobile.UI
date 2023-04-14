97884f9dd5333451f87d54f54568ca532a478355
Vetle444 <vetle_96@live.no>
2023-04-14 13:23:35 +0200

    started on pickers

---
+++ src/app/Components/ComponentsSamples/Pickers/PickersSample.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.ComponentsSamples.Pickers;
+
+public partial class PickersSample : ContentPage
+{
+    public PickersSample()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/app/Components/SampleData/SampleDataStorage.cs
@@ -0,0 +1,30 @@
+namespace Components.SampleData;
+
+public static class SampleDataStorage
+{
+    public static List<Person> People = new List<Person>()
+    {
+        new Person("Erika", "Isa"),
+        new Person("Adam", "Lehi"),
+        new Person("Emmerson", "Harve"),
+        new Person("Sylvester", "Christel"),
+        new Person("Hanne", "Dexter"),
+        new Person("Ava", "Karl"),
+        new Person("Knut", "Arne", "Johansen"),
+        new Person("Mina", "Shawn"),
+        new Person("Per", "Gunnar", "Hansen"),
+        new Person("Pablo", "Picasso",
+            "Diego José Francisco de Paula Juan Nepomuceno María de los Remedios Cipriano de la Santísima Trinidad Ruiz y"),
+        new Person("Barbara", "Sherman"),
+        new Person("Blake", "Burnett"),
+        new Person("Josiah", "Strickland"),
+        new Person("Myrtle", "Johns"),
+        new Person("Ayla", "Pitts"),
+        new Person("Faisal", "Rubio"),
+        new Person("Lexi", "Spencer"),
+        new Person("Marilyn", "Roach"),
+        new Person("Karol", "Brown"),
+        new Person("Christina","Ball")
+        
+    };
+}
