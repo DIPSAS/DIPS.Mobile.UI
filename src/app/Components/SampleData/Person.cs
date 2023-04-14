4fa93927af5b3ded7c87ff143552a642b04751b7
Vetle444 <vetle_96@live.no>
2023-04-14 13:23:35 +0200

    started on pickers

---
+++ src/app/Components/SampleData/Person.cs
@@ -0,0 +1,19 @@
+namespace Components.SampleData;
+
+public class Person
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string MiddleName { get; }
+
+    public Person(string firstName, string lastName, string middleName = null)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        MiddleName = middleName ?? string.Empty;
+    }
+
+    public string DisplayName => string.IsNullOrEmpty(MiddleName)
+        ? $"{FirstName}, {LastName}"
+        : $"{FirstName} {MiddleName}, {LastName}";
+}
