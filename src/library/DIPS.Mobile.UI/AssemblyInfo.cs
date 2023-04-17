76f66632d4c4193bbb0f709fb380c35a29181368
Vetle444 <vetle_96@live.no>
2023-04-17 08:52:04 +0200

    deleted assembly line

--- src/library/DIPS.Mobile.UI/AssemblyInfo.cs
+++ src/library/DIPS.Mobile.UI/AssemblyInfo.cs
@@ -25,7 +25,3 @@ using Microsoft.Maui.Controls.Internals;
 [assembly: XmlnsDefinition("http://dips.com/mobile.ui", "DIPS.Mobile.UI.Components.Progress")]
 [assembly: XmlnsDefinition("http://dips.com/mobile.ui", "DIPS.Mobile.UI.Converters.ValueConverters")]
 [assembly: XmlnsDefinition("http://dips.com/mobile.ui", "DIPS.Mobile.UI.Converters.MultiValueConverters")]
-[assembly: XmlnsDefinition("http://dips.com/mobile.ui", "DIPS.Mobile.UI.Extensions.Markup")]
-
-
-
