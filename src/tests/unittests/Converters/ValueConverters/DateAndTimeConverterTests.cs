2d401b239b08f6e7343a9ceaac70dd76ccfa78cd
Håvard Moås <hmo@dips.no>
2023-04-26 12:50:02 +0200

    continued

---
+++ src/tests/unittests/Converters/ValueConverters/DateAndTimeConverterTests.cs
@@ -0,0 +1,87 @@
+﻿
+using DIPS.Mobile.UI.Converters.ValueConverters;
+namespace DIPS.Mobile.UI.UnitTests.Converters.ValueConverters
+{
+    [Collection("Sequential")] //This test class is using an static shared property that is used in other tests
+    public class DateAndTimeConverterTests
+    {
+        private readonly DateTime m_now = new DateTime(1990, 12, 12, 13, 00, 00);
+        private readonly DateAndTimeConverter m_dateAndTimeConverter = new DateAndTimeConverter()
+        {
+            IgnoreLocalTime = true
+        };
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(0.0)]
+        [InlineData(0.0f)]
+        [InlineData("test")]
+        public void Convert_InvalidInput_XamlParseExceptionThrown(object invalidInput)
+        {
+            Action act = () => m_dateAndTimeConverter.Convert<string>(invalidInput);
+
+            act.Should().Throw<XamlParseException>();
+        }
+
+        [Fact]
+        public void Convert_NullInput_ShouldReturnEmptyString()
+        {
+            var actual = m_dateAndTimeConverter.Convert<string>(null!);
+
+            actual.Should().Be(string.Empty);
+        }
+
+        public static IEnumerable<object[]> TestDataForShortFormat =>
+            new List<object[]>()
+            {
+                new object[] {"no", new DateTime(1991, 12, 12, 13, 12, 12), "12. des 1991 kl 13:12"},
+                new object[] {"en-us", new DateTime(1991, 12, 12, 10, 12, 12), "Dec 12th, 1991 10:12 AM"},
+                new object[] {"en-us", new DateTime(1991, 12, 12, 13, 12, 12), "Dec 12th, 1991 01:12 PM"},
+                new object[] {"en-gb", new DateTime(1991, 12, 12, 13, 12, 12), "12th Dec 1991 13:12"},
+                new object[] {"en-gb", new DateTime(1991, 12, 12, 10, 12, 00), "12th Dec 1991 10:12"},
+            };
+
+        [Theory]
+        [MemberData(nameof(TestDataForShortFormat))]
+        public void Convert_WithShortFormat_WithCulture_CorrectFormat(string cultureName, DateTime date,
+            string expected)
+        {
+            m_dateAndTimeConverter.Format = DateAndTimeConverter.DateAndTimeConverterFormat.Short;
+
+            var actual = m_dateAndTimeConverter.Convert<string>(date, new CultureInfo(cultureName));
+
+            actual.Should().Be(expected);
+        }
+
+        public static IEnumerable<object[]> TestDataForTextFormat =>
+            new List<object[]>()
+            {
+                new object[] {"en-gb", new DateTime(1990, 12, 12, 13, 00, 00), "Today 13:00"},
+                new object[] {"en-gb", new DateTime(1991, 12, 10, 13, 00, 00), "10th Dec 13:00"},
+                new object[] {"en-gb", new DateTime(1990, 12, 12, 13, 00, 00).AddDays(-1), "Yesterday 13:00"},
+                new object[] {"en-gb", new DateTime(1990, 12, 12, 13, 00, 00).AddDays(1), "Tomorrow 13:00"},
+                new object[] {"en-us", new DateTime(1990, 12, 12, 13, 00, 00), "Today 01:00 PM"},
+                new object[] {"en-us", new DateTime(1991, 12, 10, 13, 00, 00), "Dec 10th, 01:00 PM"},
+                new object[] {"en-us", new DateTime(1990, 12, 12, 13, 00, 00).AddDays(-1), "Yesterday 01:00 PM"},
+                new object[] {"en-us", new DateTime(1990, 12, 12, 13, 00, 00).AddDays(1), "Tomorrow 01:00 PM"},
+                new object[] {"no", new DateTime(1990, 12, 12, 13, 00, 00), "I dag, kl 13:00"},
+                new object[] {"no", new DateTime(1990, 12, 12, 13, 00, 00).AddDays(-1), "I går, kl 13:00"},
+                new object[] {"no", new DateTime(1990, 12, 12, 13, 00, 00).AddDays(1), "I morgen, kl 13:00"},
+                new object[] {"no", new DateTime(1990, 12, 10, 13, 00, 00), "10. des kl 13:00"}
+            };
+
+        [Theory]
+        [MemberData(nameof(TestDataForTextFormat))]
+        public void Convert_WithTextFormat_WithDate_WithCulture_CorrectFormat(string cultureName, DateTime date,
+            string expected)
+        {
+            Clock.OverrideClock(m_now);
+
+            m_dateAndTimeConverter.Format = DateAndTimeConverter.DateAndTimeConverterFormat.Text;
+            DUILocalizedStrings.Culture = new CultureInfo(cultureName); //To force localized strings
+
+            var actual = m_dateAndTimeConverter.Convert<string>(date, DUILocalizedStrings.Culture);
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ src/tests/unittests/Converters/ValueConverters/InvertedBoolConverterTests.cs
@@ -0,0 +1,45 @@
+﻿using DIPS.Mobile.UI.Converters.ValueConverters;
+
+namespace DIPS.Mobile.UI.UnitTests.Converters.ValueConverters
+{
+    public class InvertedBoolConverterTests
+    {
+        private readonly InvertedBoolConverter m_invertedBoolConverter;
+
+        public InvertedBoolConverterTests()
+        {
+            m_invertedBoolConverter = new InvertedBoolConverter();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Convert_BooleanValue_InvertedResult(bool value)
+        {
+            var result = m_invertedBoolConverter.Convert(value, null!, null!, null!);
+            result.Should().Be(!value);
+        }
+
+
+        [Theory]
+        [InlineData("Not a bool")]
+        [InlineData(0)]
+        [InlineData(0.1)]
+        [InlineData(null)]
+        public void Convert_ValueIsNull_XamlParseExceptionThrown(object value)
+        {
+            Action act = () => m_invertedBoolConverter.Convert(value, null!, null!, null!);
+
+            act.Should().Throw<XamlParseException>();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ConvertBack_BooleanValue_IsNotInverted(bool value)
+        {
+            var result = m_invertedBoolConverter.ConvertBack(value, null!, null!, null!);
+            result.Should().Be(value);
+        }
+    }
+}
