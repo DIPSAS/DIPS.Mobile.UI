4276a7af5e666c68df9d4db44b3dcfe05fbd778c
Håvard Moås <hmo@dips.no>
2023-04-27 08:48:51 +0200

    made sure unit tests can run

---
+++ src/library/DIPS.Mobile.UI/Components/ContextMenus/dotnet/ContextMenuPlatformEffect.cs
@@ -0,0 +1,10 @@
+using Microsoft.Maui.Controls.Platform;
+
+namespace DIPS.Mobile.UI.Components.ContextMenus;
+
+public partial class ContextMenuPlatformEffect
+{
+    protected override partial void OnAttached(){}
+    protected override partial void OnDetached(){}
+
+}
