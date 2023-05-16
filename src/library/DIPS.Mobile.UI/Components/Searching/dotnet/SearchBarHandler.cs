dfc3d7d00aac890b2079830564a4e4c8d5d2a9f4
Håvard Moås <hmo@dips.no>
2023-05-16 15:41:25 +0200

    made sure it compiles for net7

--- src/library/DIPS.Mobile.UI/Components/Searching/dotnet/SearchBarHandler.cs
+++ src/library/DIPS.Mobile.UI/Components/Searching/dotnet/SearchBarHandler.cs
@@ -6,7 +6,28 @@ namespace DIPS.Mobile.UI.Components.Searching;
 
 internal partial class  SearchBarHandler : ViewHandler<SearchBar, Microsoft.Maui.Controls.View>
 {
-    public SearchBarHandler(IPropertyMapper mapper, CommandMapper? commandMapper = null) : base(mapper, commandMapper) => throw new Only_Here_For_UnitTests();
-
+    private partial void Construct() => throw new Only_Here_For_UnitTests();
     protected override View CreatePlatformView() => throw new Only_Here_For_UnitTests();
+    
+    private static void MapText(SearchBarHandler handler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+
+   private static void MapPlaceholderColor(SearchBarHandler handler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+
+    private static void MapTextColor(SearchBarHandler handler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+
+    private static void MapPlaceholder(SearchBarHandler handler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+
+    private static void MapHasBusyIndication(SearchBarHandler handler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+
+    private static void MapiOSSearchFieldBackgroundColor(SearchBarHandler searchBarHandler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+    
+    private static void MapBarColor(SearchBarHandler handler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+
+
+    private static void MapIsBusy(SearchBarHandler searchBarHandler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+
+    private static void MapIconsColor(SearchBarHandler searchBarHandler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+    
+    private static void MapHasCancelButton(SearchBarHandler searchBarHandler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
+    private static void MapCancelButtonTextColor(SearchBarHandler searchBarHandler, SearchBar searchBar) => throw new Only_Here_For_UnitTests();
 }
