874efe474b8deb680f9b1d5d70b285dac28e0704
Håvard Moås <hmo@dips.no>
2023-05-23 08:48:51 +0200

    added readme
    added default spacing for a collectionview

--- src/library/DIPS.Mobile.UI/Components/Lists/CollectionView.cs
+++ src/library/DIPS.Mobile.UI/Components/Lists/CollectionView.cs
@@ -10,5 +10,9 @@ public class CollectionView : Microsoft.Maui.Controls.CollectionView
         //Adds a extra space in the bottom to make sure the last item is not placed at the very bottom of the page, this makes the last item more accessible for people.
         Footer = new BoxView() { HeightRequest = UI.Resources.Sizes.Sizes.GetSize(SizeName.size_24) };
         SelectionMode = SelectionMode.None;
+        ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
+        {
+            ItemSpacing = UI.Resources.Sizes.Sizes.GetSize(SizeName.size_2)
+        };
     }
 }
