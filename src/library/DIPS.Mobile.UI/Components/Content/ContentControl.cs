1272be236d96c11f136d59e80a9df9e12c97465f
Håvard Moås <hmo@dips.no>
2023-05-25 10:35:40 +0200

    started on it

---
+++ src/library/DIPS.Mobile.UI/Components/Content/ContentControl.cs
@@ -0,0 +1,52 @@
+﻿namespace DIPS.Mobile.UI.Components.Content
+{
+    /// <summary>
+    /// Controls what content to display based on a <see cref="SelectorItem"/> with a <see cref="TemplateSelector"/>
+    /// </summary>
+    public class ContentControl : ContentView
+    {
+        /// <summary>
+        /// Creates an instance to use a DataTemplateSelector
+        /// </summary>
+        public ContentControl()
+        {
+            BindingContextChanged += (s, e) => UpdateContent();
+        }
+
+        private void UpdateContent()
+        {
+            if(BindingContext == null || TemplateSelector == null)
+            {
+                return;
+            }
+            
+            Content = TemplateSelector.SelectTemplate(SelectorItem ?? BindingContext, this).CreateContent() as View;
+        }
+
+        /// <summary>
+        /// The template selector used to select a template. The item passed to the <see cref="TemplateSelector.SelectTemplate"/> is <see cref="SelectorItem"/> or BindingContext if <see cref="SelectorItem"/> is not set. 
+        /// </summary>
+        public DataTemplateSelector? TemplateSelector { get; set; }
+
+
+        /// <summary>
+        ///  <see cref="SelectorItem" />
+        /// </summary>
+        public static readonly BindableProperty SelectorItemProperty = BindableProperty.Create(
+            nameof(SelectorItem),
+            typeof(object),
+            typeof(ContentControl),
+            null,
+            BindingMode.OneWay,
+            propertyChanged: (s, o, n) => ((ContentControl)s).UpdateContent());
+
+        /// <summary>
+        /// Used as the item for the <see cref="TemplateSelector"/> when selecting an item
+        /// </summary>
+        public object? SelectorItem
+        {
+            get => (object)GetValue(SelectorItemProperty);
+            set => SetValue(SelectorItemProperty, value);
+        }
+    }
+}
