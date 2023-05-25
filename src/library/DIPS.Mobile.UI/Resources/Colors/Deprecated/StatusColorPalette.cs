836ef5ef571b70f147b07fe2effc2fe921537b19
Håvard Moås <hmo@dips.no>
2023-05-25 14:51:15 +0200

    added obsolete for statics in status colors palette

--- src/library/DIPS.Mobile.UI/Resources/Colors/Deprecated/StatusColorPalette.cs
+++ src/library/DIPS.Mobile.UI/Resources/Colors/Deprecated/StatusColorPalette.cs
@@ -3,31 +3,50 @@
     public static class StatusColorPalette
     {
 
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_error_dark)}", true)]
         public static Color DangerDark = Color.FromHex("#C9524D");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_obsolete_danger)}", true)]
         public static Color Danger = Color.FromHex("#F76D6D");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_obsolete_danger_light)}", true)]
         public static Color DangerLight = Color.FromHex("#E19D9A");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_error_light)}", true)]
         public static Color DangerAir = Color.FromHex("#FFDEDE");
 
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_mandatory_dark)}", true)]
         public static Color WarningDark = Color.FromHex("#D29F0F");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_attention_dark)}", true)]
         public static Color Warning = Color.FromHex("#FFDC52");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_mandatory_light)}", true)]
         public static Color WarningLight = Color.FromHex("#EDD5A6");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_attention_light)}", true)]
         public static Color WarningAir = Color.FromHex("#F3E9BC");
 
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_success_dark)}", true)]
         public static Color SuccessDark = Color.FromHex("#006700");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_obsolete_success)}", true)]
         public static Color Success = Color.FromHex("#5CB85C");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_success_light)}", true)]
         public static Color SuccessLight = Color.FromHex("#9FD99F");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_success_light)}", true)]
         public static Color SuccessAir = Color.FromHex("#CCEBCC");
 
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_information_dark)}", true)]
         public static Color InfoDark = Color.FromHex("#266B89");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_information_dark)}", true)]
         public static Color Info = Color.FromHex("#337AB7");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_information_light)}", true)]
         public static Color InfoLight = Color.FromHex("#7FAFD8");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_information_light)}", true)]
         public static Color InfoAir = Color.FromHex("#C4DBEF");
 
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_neutral_60)}", true)]
         public static Color IdleDark = Color.FromHex("#697577");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_neutral_50)}", true)]
         public static Color Idle = Color.FromHex("#92A1A3");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_neutral_40)}", true)]
         public static Color IdleLight = Color.FromHex("#B1BEBF");
+        [Obsolete($"Use dui:Colors {nameof(ColorName.color_neutral_30)}", true)]
         public static Color IdleAir = Color.FromHex("#D3DDDE");
-
         
         public enum Identifier
         {
