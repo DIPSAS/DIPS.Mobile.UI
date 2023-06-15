9648baf9f86e4f4544dc277612ba552c9439e53b
Vetle444 <vetle_96@live.no>
2023-06-15 15:07:31 +0200

    finish

--- src/library/DIPS.Mobile.UI/Components/Images/Image/Android/ImageHandler.cs
+++ src/library/DIPS.Mobile.UI/Components/Images/Image/Android/ImageHandler.cs
@@ -1,10 +1,12 @@
+using Microsoft.Maui.Platform;
+
 namespace DIPS.Mobile.UI.Components.Images.Image;
 
 public partial class ImageHandler
 {
     private static partial void TrySetTintColor(ImageHandler handler, Image image)
     {
-        
+        handler.PlatformView.SetColorFilter(image.TintColor.ToPlatform());
     }
 
 }
