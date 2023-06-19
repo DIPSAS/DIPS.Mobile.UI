8a4480ee2fdcac372a141666e934fcbd94545fb7
runner <runner@Mac-1687168170432.local>
2023-06-19 09:57:04 +0000

    'Generated'

--- src/library/DIPS.Mobile.UI/Resources/Icons/IconName.cs
+++ src/library/DIPS.Mobile.UI/Resources/Icons/IconName.cs
@@ -1,20 +1,31 @@
 /*
 Do not edit directly,
-generated 6/15/2023 6:32:20 AM from DIPS.Mobile.DesignTokens
+generated 6/19/2023 9:53:11 AM from DIPS.Mobile.DesignTokens
 */
 
 namespace  DIPS.Mobile.UI.Resources.Icons
 {
     public enum IconName
     {
-        information_line, 
+        weight_line, 
+information_line, 
+document_line, 
+beaker_fill, 
+barcode_fill, 
 bell_fill, 
 descending_fill, 
+patient_list_fill, 
 schedule_fill, 
+document_fill, 
+respiration_line, 
+heartrate_line, 
 ascending_fill, 
 home_fill, 
 settings_fill, 
+internalmessage_fill, 
 tasklist_line, 
-arrow_right_s_line
+search_line, 
+arrow_right_s_line, 
+comments_line
     }
 }
