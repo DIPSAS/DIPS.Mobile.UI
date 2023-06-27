03a1c66b0fb44992b182d498ced64e57b15431cb
Håvard Moås <hmo@dips.no>
2023-06-27 15:18:37 +0200

    Fixed android issue with collection view inside modal pages when having nav bar

--- src/library/DIPS.Mobile.UI/Effects/Modal/Android/ModalPlatformEffect.cs
+++ src/library/DIPS.Mobile.UI/Effects/Modal/Android/ModalPlatformEffect.cs
@@ -8,22 +8,22 @@ namespace DIPS.Mobile.UI.Effects.Modal.Android;
 
 public class ModalPlatformEffect : PlatformEffect
 {
-    protected override void OnAttached()
+    protected override void OnAttached()ModalPlatformEffect
     {
         if (Element is not ContentPage contentPage)
             return;
 
-        var verticalStackLayout = new VerticalStackLayout();
+        var grid = new Grid(){RowDefinitions = new RowDefinitionCollection(){new(GridLength.Auto), new(GridLength.Star)}};
 
         var toolbar = new DUIToolbar 
         { 
             PageConnectedTo = contentPage, 
             BackgroundColor = Colors.GetColor(ColorName.color_primary_90),
         };
-        verticalStackLayout.Add(toolbar);
-        verticalStackLayout.Add(new ContentView { Content = contentPage.Content, Padding = contentPage.Padding });
+        grid.Add(toolbar, 0, 0);
+        grid.Add(new ContentView { Content = contentPage.Content, Padding = contentPage.Padding }, 0, 1);
         contentPage.Padding = 0;
-        contentPage.Content = verticalStackLayout;
+        contentPage.Content = grid;
     }
 
     protected override void OnDetached()
