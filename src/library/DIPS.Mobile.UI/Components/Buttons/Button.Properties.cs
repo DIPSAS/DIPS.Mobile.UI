c639c21a07bbdaace286e865a9796f063e9096bc
Håvard Moås <hmo@dips.no>
2023-08-04 10:54:14 +0200

    removed properties that are not in use

--- src/library/DIPS.Mobile.UI/Components/Buttons/Button.Properties.cs
+++ src/library/DIPS.Mobile.UI/Components/Buttons/Button.Properties.cs
@@ -15,16 +15,5 @@ namespace DIPS.Mobile.UI.Components.Buttons
             get => (Thickness)GetValue(AdditionalHitBoxSizeProperty);
             set => SetValue(AdditionalHitBoxSizeProperty, value);
         }
-
-        public static readonly BindableProperty BackgroundColorAlphaProperty = BindableProperty.Create(
-            nameof(BackgroundColorAlpha),
-            typeof(float?),
-            typeof(Button), defaultValue:null);
-
-        public float? BackgroundColorAlpha
-        {
-            get => (float?)GetValue(BackgroundColorAlphaProperty);
-            set => SetValue(BackgroundColorAlphaProperty, value);
-        }
     }
 }
--- src/library/DIPS.Mobile.UI/Components/Buttons/Button.cs
+++ src/library/DIPS.Mobile.UI/Components/Buttons/Button.cs
@@ -9,31 +9,5 @@ namespace DIPS.Mobile.UI.Components.Buttons
             Padding = new Thickness(Sizes.GetSize(SizeName.size_3), Sizes.GetSize(SizeName.size_2));
             CornerRadius = Sizes.GetSize(SizeName.size_2);
         }
-
-        protected override void OnSizeAllocated(double width, double height)
-        {
-            base.OnSizeAllocated(width, height);
-            // TrySetBackgroundColorAlpha();
-        }
-
-        protected override void OnPropertyChanged(string propertyName = null)
-        {
-            base.OnPropertyChanged(propertyName);
-            if (propertyName.Equals(nameof(BackgroundColor)))
-            {
-                // TrySetBackgroundColorAlpha();
-            }
-        }
-
-        private void TrySetBackgroundColorAlpha()
-        {
-            if (BackgroundColorAlpha != null) //Set background alpha if it specifically set
-            {
-                var oldBackgroundColor = BackgroundColor;
-                BackgroundColor = new Color(oldBackgroundColor.Red, oldBackgroundColor.Green,
-                    oldBackgroundColor.Blue,
-                    (float)BackgroundColorAlpha);
-            }
-        }
     }
 }
