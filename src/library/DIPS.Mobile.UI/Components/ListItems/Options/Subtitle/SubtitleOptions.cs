6feb65224d59fbd8dcd6dc945208adbe5188b6bc
Vetle444 <vetle_96@live.no>
2023-08-15 09:00:31 +0200

    Changed subtitle font size and changed background color on some pages

--- src/library/DIPS.Mobile.UI/Components/ListItems/Options/Subtitle/SubtitleOptions.cs
+++ src/library/DIPS.Mobile.UI/Components/ListItems/Options/Subtitle/SubtitleOptions.cs
@@ -12,7 +12,7 @@ public partial class SubtitleOptions : BindableObject, IListItemOptions
         listItem.SubtitleLabel.SetBinding(Label.FontAttributesProperty, new Binding(nameof(FontAttributes), source: this));
         listItem.SubtitleLabel.SetBinding(Label.HorizontalTextAlignmentProperty, new Binding(nameof(HorizontalTextAlignment), source: this));
         listItem.SubtitleLabel.SetBinding(Label.VerticalTextAlignmentProperty, new Binding(nameof(VerticalTextAlignment), source: this));
-        listItem.SubtitleLabel.FontSize = Sizes.GetSize(SizeName.size_2);
+        listItem.SubtitleLabel.FontSize = Sizes.GetSize(SizeName.size_3);
         listItem.SubtitleLabel.TextColor = Colors.GetColor(ColorName.color_neutral_60);
 
     }
