ca9a2612fb9b7d42a30cbf2186c9e07bcba9f158
Håvard Moås <hmo@dips.no>
2023-08-30 10:49:31 +0200

    added try catching when we try to remove fragments for cases when the state is lost

--- src/library/DIPS.Mobile.UI/Extensions/Android/FragmentManagerExtensions.cs
+++ src/library/DIPS.Mobile.UI/Extensions/Android/FragmentManagerExtensions.cs
@@ -1,4 +1,5 @@
 using Android.App;
+using Java.Lang;
 using FragmentManager = AndroidX.Fragment.App.FragmentManager;
 
 namespace DIPS.Mobile.UI.Extensions.Android;
@@ -8,7 +9,19 @@ public static class FragmentManagerExtensions
     public static void RemoveFragmentWithTag(this FragmentManager fragmentManager, string tag)
     {
         var fragment = fragmentManager.FindFragmentByTag(tag);
-        if (fragment is not null)
+        if (fragment is null)
+        {
+            return;
+        }
+
+        try
+        {
             fragmentManager.BeginTransaction().Remove(fragment).Commit();
+        }
+        catch (IllegalStateException ignored) //https://stackoverflow.com/a/27854077, to reproduce this : Debug (start) the app but keep the phone locked (blackscreen).
+        {
+            fragmentManager.BeginTransaction().Remove(fragment).CommitAllowingStateLoss();
+        }
+
     }
 }
