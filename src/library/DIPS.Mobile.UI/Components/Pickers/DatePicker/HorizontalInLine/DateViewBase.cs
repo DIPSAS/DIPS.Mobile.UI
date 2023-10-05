756199f49bbe0d5fa1ce92905d82d04e8c88bb99
Eirik Hemstad <ehe@dips.no>
2023-10-05 13:57:13 +0200

    Made base class

---
+++ src/library/DIPS.Mobile.UI/Components/Pickers/DatePicker/HorizontalInLine/DateViewBase.cs
@@ -0,0 +1,111 @@
+using DIPS.Mobile.UI.Components.Content;
+using DIPS.Mobile.UI.Components.Content.DataTemplateSelectors;
+using DIPS.Mobile.UI.Converters.ValueConverters;
+using DIPS.Mobile.UI.Extensions.Markup;
+using Colors = DIPS.Mobile.UI.Resources.Colors.Colors;
+using Label = DIPS.Mobile.UI.Components.Labels.Label;
+
+namespace DIPS.Mobile.UI.Components.Pickers.DatePicker.HorizontalInLine;
+
+/// <summary>
+/// TODO: Rewrite this so its easy to use publicly if its needed.
+/// </summary>
+public abstract class DateViewBase : Grid
+{
+    public DateViewBase()
+    {
+        Loaded += CreateView;
+    }
+
+    private void CreateView(object? sender, EventArgs e)
+    {
+        RowSpacing = Sizes.GetSize(SizeName.size_2);
+        // RowDefinitions =
+        //     new RowDefinitionCollection(new RowDefinition(GridLength.Auto), new RowDefinition(GridLength.Auto),
+        //         new RowDefinition(GridLength.Auto));
+
+        SetBinding(BackgroundColorProperty,
+            new Binding(nameof(SelectableDateViewModel.IsSelected),
+                converter: new BoolToObjectConverter()
+                {
+                    TrueObject = Colors.GetColor(ColorName.color_primary_90),
+                    FalseObject = Colors.GetColor(ColorName.color_neutral_05)
+                }));
+        
+        
+        //Month header
+        this.Add(CreateMonthHeaderContentControl(), 0, 0);
+
+        //Day number label
+        var dayLabel = CreateLabel(new Label());
+        dayLabel.SetBinding(Microsoft.Maui.Controls.Label.TextColorProperty,
+            new Binding(nameof(SelectableDateViewModel.IsSelected),
+                converter: new BoolToObjectConverter()
+                {
+                    TrueObject = Colors.GetColor(ColorName.color_system_white),
+                    FalseObject = Colors.GetColor(ColorName.color_system_black),
+                }));
+        dayLabel.SetBinding(Label.TextProperty,
+            new Binding(nameof(SelectableDateViewModel.Day)));
+
+        this.Add(dayLabel, 0, 1);
+
+        
+        Loaded -= CreateView;
+    }
+    
+    protected virtual void OnViewCreated() {}
+
+    private ContentControl CreateMonthHeaderContentControl()
+    {
+        var contentControl = new ContentControl();
+        contentControl.Padding = new Thickness(Sizes.GetSize(SizeName.size_2));
+        
+        contentControl.SetBinding(BackgroundColorProperty,
+            new Binding(nameof(SelectableDateViewModel.IsSelected),
+                converter: new BoolToObjectConverter()
+                {
+                    TrueObject = Colors.GetColor(ColorName.color_secondary_90),
+                    FalseObject = Colors.GetColor(ColorName.color_neutral_05)
+                }));
+        contentControl.SetBinding(ContentControl.SelectorItemProperty,
+            new Binding(nameof(SelectableDateViewModel.IsCurrentYear)));
+        contentControl.TemplateSelector = new BooleanDataTemplateSelector()
+        {
+            TrueTemplate = new DataTemplate(() =>
+            {
+                var monthLabel = CreateLabel(new Label() {FontSize = Sizes.GetSize(SizeName.size_4)});
+                monthLabel.HorizontalTextAlignment = TextAlignment.Center;
+                monthLabel.TextTransform = TextTransform.Uppercase;
+                monthLabel.SetBinding(Microsoft.Maui.Controls.Label.TextProperty,
+                    new Binding(nameof(SelectableDateViewModel.MonthName)));
+                return monthLabel;
+            }),
+            FalseTemplate = new DataTemplate(() =>
+            {
+                var monthAndYearLabel = CreateLabel(new Label() {FontSize = Sizes.GetSize(SizeName.size_4)});
+                var monthNameSpan =
+                    new Span() {FontSize = Sizes.GetSize(SizeName.size_4), TextTransform = TextTransform.Uppercase};
+                monthNameSpan.SetBinding(Span.TextProperty,
+                    new Binding(nameof(SelectableDateViewModel.MonthName),
+                        converter: new StringCaseConverter() {StringCase = StringCase.Upper}));
+                var blankSpan = new Span() {Text = " "};
+                var yearNameSpan = new Span() {FontSize = Sizes.GetSize(SizeName.size_4)};
+                yearNameSpan.SetBinding(Span.TextProperty,
+                    new Binding(nameof(SelectableDateViewModel.YearName)));
+                monthAndYearLabel.FormattedText =
+                    new FormattedString() {Spans = {monthNameSpan, blankSpan, yearNameSpan}};
+                return monthAndYearLabel;
+            })
+        };
+        return contentControl;
+    }
+
+    protected static Label CreateLabel(Label theLabel, SizeName size = SizeName.size_4)
+    {
+        theLabel.FontSize = Sizes.GetSize(size);
+        theLabel.HorizontalTextAlignment = TextAlignment.Center;
+        theLabel.VerticalTextAlignment = TextAlignment.Center;
+        return theLabel;
+    }
+}
