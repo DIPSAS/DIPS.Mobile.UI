3d3033197fe05502edd92aa14cc45f571385b6bd
Håvard Moås <hmo@dips.no>
2023-12-13 13:43:25 +0100

    hardcoded on android and dynamic on ios due to limitation of scroll view

--- src/library/DIPS.Mobile.UI/Components/Lists/ScrollView/ScrollView.cs
+++ src/library/DIPS.Mobile.UI/Components/Lists/ScrollView/ScrollView.cs
@@ -3,17 +3,34 @@ namespace DIPS.Mobile.UI.Components.Lists;
 public partial class ScrollView : Microsoft.Maui.Controls.ScrollView
 {
     private bool m_hasAddedSpaceToBottom;
+    private double m_originalHeight;
 
+    public ScrollView()
+    {
+#if __ANDROID__ //Not possible to set padding on scroll view after its rendered
+        AdjustPadding(Sizes.GetSize(SizeName.size_25) * 4);
+#endif
+        
+    }
+
+#if __IOS__
     protected override void OnSizeAllocated(double width, double height)
     {
         base.OnSizeAllocated(width, height);
+        AdjustPadding(height);
+    }
+#endif
+
+
+    private void AdjustPadding(double height)
+    {
         var oldPadding = Padding;
         if (height > 0 && !m_hasAddedSpaceToBottom && HasAdditionalSpaceAtTheEnd)
         {
             m_hasAddedSpaceToBottom = true;
             var newPadding = new Thickness(oldPadding.Left, oldPadding.Top, oldPadding.Right,
-                oldPadding.Bottom + (height / 2));
-            Padding = newPadding;    
+                (int)(oldPadding.Bottom + (height / 2)));
+            Padding = newPadding;
         }
     }
 }
