9ac3ed7739746f1ac1126945d2e5c29e5154c769
Håvard Moås <hmo@dips.no>
2024-01-15 23:48:56 +0100

    added comment for divider

--- src/library/DIPS.Mobile.UI/Components/Dividers/Divider.cs
+++ src/library/DIPS.Mobile.UI/Components/Dividers/Divider.cs
@@ -7,6 +7,7 @@ public class Divider : ContentView
     public Divider()
     {
         this.SetAppThemeColor(BackgroundColorProperty, ColorName.color_neutral_20);
+        //Was previously using Line component, but it has memory leaks. Changing to Border until its fixed.
         var divider = new Border() {HeightRequest = 1.2};
         divider.SetBinding(BackgroundProperty, new Binding(nameof(BackgroundColor), source: this));
         Content = divider;
