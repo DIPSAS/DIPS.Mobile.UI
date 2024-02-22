8da967687b04f6fcc15f892712dbf174e7c7787e
Håvard Moås <hmo@dips.no>
2024-02-22 10:48:44 +0100

    made sure we use the correct camera and added tap to touch

--- src/library/DIPS.Mobile.UI/API/Camera/BarcodeScanning/iOS/BarcodeScanner.cs
+++ src/library/DIPS.Mobile.UI/API/Camera/BarcodeScanning/iOS/BarcodeScanner.cs
@@ -1,17 +1,23 @@
-#nullable enable
 using AVFoundation;
 using CoreAnimation;
 using CoreFoundation;
 using CoreGraphics;
-using DIPS.Mobile.UI.API.Vibration;
-using Microsoft.Maui.Handlers;
+using Foundation;
 using UIKit;
+using ContentView = Microsoft.Maui.Platform.ContentView;
 
 namespace DIPS.Mobile.UI.API.Camera.BarcodeScanning;
 
 public partial class BarcodeScanner
 {
+    //The session of the capture, there can only be one capture session running in an iOS app.
     private AVCaptureSession? m_captureSession;
+    //The the lense to be used for scanning bar codes
+    private AVCaptureDevice? m_captureDevice;
+    //The rectangle that people see in the preview which we will focus on scanning bar codes in
+    private CGRect? m_rectOfInterest;
+    private ContentView? m_previewUIView;
+    private UITapToFocusGestureRecognizer? m_tapToFocusGestureRecognizer;
 
 
     //https://developer.apple.com/documentation/avfoundation/capture_setup/requesting_authorization_to_capture_and_save_media#2958841
@@ -40,6 +46,17 @@ public partial class BarcodeScanner
                 m_captureSession = null;
             });
         }
+
+        m_captureDevice = null;
+        m_rectOfInterest = null;
+        
+        if (m_tapToFocusGestureRecognizer != null)
+        {
+            m_tapToFocusGestureRecognizer.RemoveReferences();
+            m_previewUIView?.RemoveGestureRecognizer(m_tapToFocusGestureRecognizer);
+        }
+
+        m_previewUIView = null;
     }
 
     internal async partial Task PlatformStart()
@@ -47,33 +64,19 @@ public partial class BarcodeScanner
         //This makes sure we display the video feed
         if (m_preview?.Handler is not PreviewHandler previewHandler) return;
 
+        m_previewUIView = previewHandler.PlatformView;
+        
         m_captureSession = new AVCaptureSession();
         //Call beginConfiguration() before changing a session’s inputs or outputs, and call commitConfiguration() after making changes.
         m_captureSession.BeginConfiguration();
 
         var previewLayer =
             await previewHandler.WaitForViewLayoutAndAddSessionToPreview(m_captureSession, AVLayerVideoGravity.ResizeAspectFill);
-        //Add camera input: https://developer.apple.com/documentation/avfoundation/capture_setup/choosing_a_capture_device#2958868
-        var captureDeviceDiscoverySession = AVCaptureDeviceDiscoverySession.Create(
-            new[] {AVCaptureDeviceType.BuiltInWideAngleCamera,}, AVMediaTypes.Video,
-            AVCaptureDevicePosition.Unspecified);
-        AVCaptureDevice? captureDevice;
-        if (captureDeviceDiscoverySession.Devices.Length > 0)
-        {
-            //TODO: Research what devices are best for bar code scanning
-            captureDevice = captureDeviceDiscoverySession.Devices[0];
-            if (captureDeviceDiscoverySession.Devices.Length > 1)
-            {
-                Log(
-                    $@"Found {captureDeviceDiscoverySession.Devices.Length} devices to pick from. We have selected the best bar code camera: {captureDevice.DeviceType}");
-            }
-        }
-        else
-        {
-            throw new Exception($"Found no cameras to use on the back of the iPhone");
-        }
+        //Choosing build in wide angle camera, same as the sample app from Apple: AVCamBarCode: https://developer.apple.com/documentation/avfoundation/capture_setup/avcambarcode_detecting_barcodes_and_faces
+        m_captureDevice = AVCaptureDevice.GetDefaultDevice(AVCaptureDeviceType.BuiltInWideAngleCamera,
+            AVMediaTypes.Video, AVCaptureDevicePosition.Back);
 
-        var videoDeviceInput = AVCaptureDeviceInput.FromDevice(captureDevice);
+        var videoDeviceInput = AVCaptureDeviceInput.FromDevice(m_captureDevice);
         if (videoDeviceInput == null) throw new Exception($"video device input is null");
         if (m_captureSession.CanAddInput(videoDeviceInput))
         {
@@ -130,17 +133,14 @@ public partial class BarcodeScanner
             var y = (previewLayer.Frame.Height / 2) - (height / 2);
             var width = previewLayer.Frame.Width;
             var cgRect = new CGRect(x, y, width, height);
-            captureMetadataOutput.RectOfInterest = previewLayer.MapToMetadataOutputCoordinates(cgRect);
-
-            if (m_preview?.Handler is ContentViewHandler previewViewHandler)
-            {
-                var uiView = previewViewHandler.PlatformView;
-                var layer = new CALayer();
-                layer.Frame = cgRect;
-                layer.BorderColor = UIColor.White.CGColor;
-                layer.BorderWidth = 2;
-                uiView.Layer.AddSublayer(layer);
-            }
+            m_rectOfInterest = captureMetadataOutput.RectOfInterest = previewLayer.MapToMetadataOutputCoordinates(cgRect);
+            
+            var layer = new CALayer();
+            layer.Frame = cgRect;
+            layer.BorderColor = UIColor.White.CGColor;
+            layer.BorderWidth = 2;
+            m_previewUIView.Layer.AddSublayer(layer);
+            
         }
         else
         {
@@ -150,8 +150,8 @@ public partial class BarcodeScanner
         //Commit the configuration
         m_captureSession.CommitConfiguration();
 
-        SetBarCodeMinimumZoom(captureDevice, captureMetadataOutput.RectOfInterest);
-        previewHandler.AddZoomSlider(captureDevice);
+        SetBarCodeMinimumZoom(m_captureDevice, captureMetadataOutput.RectOfInterest);
+        previewHandler.AddZoomSlider(m_captureDevice);
 
         await Task.Run(() =>
             {
@@ -165,19 +165,43 @@ public partial class BarcodeScanner
                 }
             }
         );
+        
+        m_tapToFocusGestureRecognizer = new UITapToFocusGestureRecognizer(TapToFocus);
+        m_previewUIView.AddGestureRecognizer(m_tapToFocusGestureRecognizer);
+    }
 
-        m_preview?.GestureRecognizers.Add(new TapGestureRecognizer()
+    private void TapToFocus(NSSet touches, UIEvent uiEvent)
+    {
+        if (m_previewUIView == null) return;
+        if (touches.First() is not UITouch touchPoint) return;
+        var screenSize = m_previewUIView.Bounds.Size;
+        var focusPoint = new CGPoint(x: touchPoint.LocationInView(m_previewUIView).Y / screenSize.Height,
+            y: 1.0 - touchPoint.LocationInView(m_previewUIView).X / screenSize.Width);
+
+        if (m_captureDevice != null)
         {
-            Command = new Command(() =>
+            if (m_captureDevice.LockForConfiguration(out var error))
             {
-                captureDevice.LockForConfiguration(out var error);
-                var x = captureMetadataOutput.RectOfInterest.X - captureMetadataOutput.RectOfInterest.Width;
-                var y = captureMetadataOutput.RectOfInterest.Y - captureMetadataOutput.RectOfInterest.Height;
-                captureDevice.FocusPointOfInterest = new CGPoint(x, y);
-                captureDevice.FocusMode = AVCaptureFocusMode.AutoFocus;
-                captureDevice.UnlockForConfiguration();
-            })
-        });
+                if (!string.IsNullOrEmpty(error.ToString()))
+                {
+                    Log(error.ToString());
+                }
+
+                if (m_captureDevice.FocusPointOfInterestSupported)
+                {
+                    m_captureDevice.FocusPointOfInterest = focusPoint;
+                    m_captureDevice.FocusMode = AVCaptureFocusMode.AutoFocus;
+                }
+
+                if (m_captureDevice.ExposurePointOfInterestSupported)
+                {
+                    m_captureDevice.ExposurePointOfInterest = focusPoint;
+                    m_captureDevice.ExposureMode = AVCaptureExposureMode.AutoExpose;
+                }
+
+                m_captureDevice.UnlockForConfiguration();
+            }
+        }
     }
 
     //Taken from: https://developer.apple.com/wwdc21/10047?time=117
@@ -221,6 +245,27 @@ public partial class BarcodeScanner
                 throw;
             }
         }
+    }
+}
+
+internal class UITapToFocusGestureRecognizer : UIGestureRecognizer
+{
+    private Action<NSSet, UIEvent>? m_onTouchesBegan;
+
+    public UITapToFocusGestureRecognizer(Action<NSSet, UIEvent> onTouchesBegan)
+    {
+        m_onTouchesBegan = onTouchesBegan;
+    }
+    
+    public override void TouchesBegan(NSSet touches, UIEvent evt)
+    {
+        m_onTouchesBegan?.Invoke(touches, evt);
+        base.TouchesBegan(touches, evt);
+    }
+
+    internal void RemoveReferences()
+    {
+        m_onTouchesBegan = null;
     }
 }
 
