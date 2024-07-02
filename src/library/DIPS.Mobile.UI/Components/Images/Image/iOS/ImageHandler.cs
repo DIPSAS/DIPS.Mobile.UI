b5df47266d3fefb5e7776d2c02f5232f93645186
Vetle444 <vetle_96@live.no>
2024-07-02 15:30:19 +0200

    trycatch

--- src/library/DIPS.Mobile.UI/Components/Images/Image/iOS/ImageHandler.cs
+++ src/library/DIPS.Mobile.UI/Components/Images/Image/iOS/ImageHandler.cs
@@ -8,20 +8,27 @@ public partial class ImageHandler
 
     private static async partial void TrySetTintColor(ImageHandler handler, Image image)
     {
-        var tries = 0;
-        // Wait for Image to be set
-        while (handler.PlatformView.Image is null)
+        try
         {
-            if(tries > 10)
-                return;
+            var tries = 0;
+            // Wait for Image to be set
+            while (handler.PlatformView.Image is null)
+            {
+                if(tries > 10)
+                    return;
+                
+                await Task.Delay(1);
+                tries++;
+            }
             
-            await Task.Delay(1);
-            tries++;
+            if (image.TintColor == null) return;
+            handler.PlatformView.Image = handler.PlatformView.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+            handler.PlatformView.TintColor = image.TintColor.ToPlatform();
         }
-        
-        if (image.TintColor == null) return;
-        handler.PlatformView.Image = handler.PlatformView.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
-        handler.PlatformView.TintColor = image.TintColor.ToPlatform();
+        catch
+        {
+            // If it doesn't work, it doesn't work
+        }
     }
 
     private static partial void AppendToPropertyMapper()
