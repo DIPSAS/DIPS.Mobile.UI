c8de9b82cb94d493b2186c3b2e6682b4add4dae7
Vetle444 <vetle_96@live.no>
2024-08-19 10:14:36 +0200

    yas

--- src/library/DIPS.Mobile.UI/Components/Pickers/NullableDatePicker/NullableDatePicker.Properties.cs
+++ src/library/DIPS.Mobile.UI/Components/Pickers/NullableDatePicker/NullableDatePicker.Properties.cs
@@ -10,67 +10,10 @@ public partial class NullableDatePicker
         typeof(NullableDatePicker), defaultBindingMode: BindingMode.TwoWay,
         propertyChanged: (bindable, _, _) => ((NullableDatePicker)bindable).OnSelectedDateChanged());
 
-    public DateTime? SelectedDate
+    public new DateTime? SelectedDate
     {
         get => (DateTime?)GetValue(SelectedDateProperty);
         set => SetValue(SelectedDateProperty, value);
     }
 
-    public static readonly BindableProperty SelectedDateCommandProperty = BindableProperty.Create(
-        nameof(SelectedDateCommand),
-        typeof(ICommand),
-        typeof(NullableDatePicker));
-
-    /// <summary>
-    /// The command to be executed when people selected a date from the date picker.
-    /// </summary>
-    public ICommand? SelectedDateCommand
-    {
-        get => (ICommand)GetValue(SelectedDateCommandProperty);
-        set => SetValue(SelectedDateCommandProperty, value);
-    }
-
-    public static readonly BindableProperty IgnoreLocalTimeProperty = BindableProperty.Create(
-        nameof(IgnoreLocalTime),
-        typeof(bool),
-        typeof(NullableDatePicker));
-
-
-    /// <summary>
-    /// If this is false, the <see cref="NullableDatePicker"/> will use local time zone.
-    /// If this is true, the <see cref="NullableDatePicker"/> will use UTC time zone.
-    /// </summary>
-    public bool IgnoreLocalTime
-    {
-        get => (bool)GetValue(IgnoreLocalTimeProperty);
-        set => SetValue(IgnoreLocalTimeProperty, value);
-    }
-
-    public static readonly BindableProperty MinimumDateProperty = BindableProperty.Create(
-        nameof(MinimumDate),
-        typeof(DateTime?),
-        typeof(NullableDatePicker));
-
-    /// <summary>
-    /// Sets the minimum date that people can choose
-    /// </summary>
-    public DateTime? MinimumDate
-    {
-        get => (DateTime?)GetValue(MinimumDateProperty);
-        set => SetValue(MinimumDateProperty, value);
-    }
-
-    public static readonly BindableProperty MaximumDateProperty = BindableProperty.Create(
-        nameof(MaximumDate),
-        typeof(DateTime?),
-        typeof(NullableDatePicker));
-
-    /// <summary>
-    /// Sets the maximum date that people can choose
-    /// </summary>
-    public DateTime? MaximumDate
-    {
-        get => (DateTime?)GetValue(MaximumDateProperty);
-        set => SetValue(MaximumDateProperty, value);
-    }
 }
--- src/library/DIPS.Mobile.UI/Components/Pickers/NullableDatePicker/NullableDatePicker.cs
+++ src/library/DIPS.Mobile.UI/Components/Pickers/NullableDatePicker/NullableDatePicker.cs
@@ -1,59 +1,9 @@
-using DIPS.Mobile.UI.Components.Pickers.NullableDatePickerShared;
-
 namespace DIPS.Mobile.UI.Components.Pickers.NullableDatePicker;
 
-public partial class NullableDatePicker : BaseNullableDatePicker
+public partial class NullableDatePicker : DatePicker.DatePicker
 {
-    private DatePicker.DatePicker? m_datePicker;
-    
-    protected override View CreateDateOrTimePicker()
-    {
-        m_datePicker = new DatePicker.DatePicker
-        {
-            SelectedDateCommand = new Command(OnInternalDateChanged),
-            SelectedDate = SelectedDate ?? DateTime.Now.Date
-        };
-
-        m_datePicker.SetBinding(DatePicker.DatePicker.MinimumDateProperty, new Binding(nameof(MinimumDate), BindingMode.OneWay, source: this));
-        m_datePicker.SetBinding(DatePicker.DatePicker.MaximumDateProperty, new Binding(nameof(MaximumDate), BindingMode.OneWay, source: this));
-        m_datePicker.SetBinding(DatePicker.DatePicker.IgnoreLocalTimeProperty, new Binding(nameof(IgnoreLocalTime), BindingMode.OneWay, source: this));
-        m_datePicker.SetBinding(HorizontalOptionsProperty, new Binding(nameof(HorizontalOptions), BindingMode.OneWay, source: this));
-        return m_datePicker;
-    }
-
-    protected override bool IsDateOrTimeNull()
-    {
-        return SelectedDate is null;
-    }
-    
-    private void OnInternalDateChanged()
-    {
-        SelectedDate = m_datePicker?.SelectedDate;
-    }
-
-    protected override void OnSwitchToggled(object? sender, ToggledEventArgs e)
-    {
-        base.OnSwitchToggled(sender, e);
-        
-        if (e.Value)
-        {
-            OnInternalDateChanged();
-        }
-        else
-        {
-            SelectedDate = null;
-        }
-        
-        SelectedDateCommand?.Execute(null);
-    }
-
     private void OnSelectedDateChanged()
     {
-        if (m_datePicker is not null)
-            m_datePicker.SelectedDate = SelectedDate ?? m_datePicker.SelectedDate;
-        else return;
         
-        DateEnabledSwitch.IsToggled = SelectedDate.HasValue;
-        SelectedDateCommand?.Execute(null);
     }
 }
