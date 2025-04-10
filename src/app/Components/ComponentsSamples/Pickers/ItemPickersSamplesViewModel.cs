2f58c7829eb36f9c4a5563c658ec6a4d4af5bbe0
Eirik Hemstad <ehe@dips.no>
2025-04-10 11:25:32 +0200

    Add components sample

--- src/app/Components/ComponentsSamples/Pickers/ItemPickersSamplesViewModel.cs
+++ src/app/Components/ComponentsSamples/Pickers/ItemPickersSamplesViewModel.cs
@@ -20,4 +20,24 @@ public class ItemPickersSamplesViewModel : ViewModel
         get => m_selectedItems;
         set => RaiseWhenSet(ref m_selectedItems, value);
     }
+
+    public Func<string, Person> PersonFactory { get; } = fullName =>
+    {
+        var splitNames = fullName.Split(" ");
+        var firstName = splitNames[0];
+
+        var middleName = string.Empty;
+        var lastName = string.Empty;
+        if (splitNames.Length == 2)
+        {
+            lastName = splitNames[1];
+        }
+        else if (splitNames.Length > 2)
+        {
+            middleName = splitNames[1];
+            lastName = splitNames[2];
+        }
+
+        return new Person(firstName, lastName, middleName);
+    };
 }
