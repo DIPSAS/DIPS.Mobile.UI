9f9c65e47325061d842acde985660bb06bb00737
Vetle444 <vetle_96@live.no>
2025-06-16 12:42:41 +0200

    works on android now too

--- src/library/DIPS.Mobile.UI/Components/Dividers/Android/DividerHandler.cs
+++ src/library/DIPS.Mobile.UI/Components/Dividers/Android/DividerHandler.cs
@@ -1,6 +1,32 @@
+using Android.Content;
+using Microsoft.Maui.Platform;
+
 namespace DIPS.Mobile.UI.Components.Dividers;
 
 internal partial class DividerHandler
 {
+    protected override MauiShapeView CreatePlatformView()
+    {
+        return new DividerShapeView(Context) { WeakVirtualView = new WeakReference<Divider>((Divider)VirtualView) };
+    }
+}
+
+internal class DividerShapeView : MauiShapeView
+{
+    public DividerShapeView(Context? context) : base(context)
+    {
+    }
     
+    public WeakReference<Divider>? WeakVirtualView { private get; init; }
+    
+    public Divider? VirtualView
+    {
+        get
+        {
+            if (WeakVirtualView?.TryGetTarget(out var target) ?? false)
+                return target;
+
+            return null;
+        }
+    }
 }
