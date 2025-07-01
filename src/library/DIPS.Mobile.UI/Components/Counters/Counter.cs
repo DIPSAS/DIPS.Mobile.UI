6c90d091b55134c103643395be59a1f208845bce
Eirik Hemstad <ehe@dips.no>
2025-07-01 14:35:55 +0200

    Update Counter.cs

--- src/library/DIPS.Mobile.UI/Components/Counters/Counter.cs
+++ src/library/DIPS.Mobile.UI/Components/Counters/Counter.cs
@@ -1,26 +1,13 @@
-using DIPS.Mobile.UI.API.Library;
 using DIPS.Mobile.UI.Resources.Styles;
 using DIPS.Mobile.UI.Resources.Styles.Label;
-using Microsoft.Maui.Controls.Shapes;
 using Colors = DIPS.Mobile.UI.Resources.Colors.Colors;
 
 namespace DIPS.Mobile.UI.Components.Counters;
 
 public partial class Counter : Grid
 {
-    private readonly Border m_primaryBorder = new()
-    {
-        StrokeShape = new RoundRectangle {StrokeThickness = 0},
-        Padding = 0,
-        Margin = 0
-    };
-
-    private readonly Border m_secondaryBorder = new()
-    {
-        StrokeShape = new RoundRectangle {StrokeThickness = 0},
-        Padding = 0,
-        Margin = 0
-    };
+    private readonly Label m_primaryLabel = new() {Style = Styles.GetLabelStyle(LabelStyle.Body200), Padding = new Thickness(Sizes.GetSize(SizeName.size_2), 0), BackgroundColor = Colors.GetColor(ColorName.color_fill_subtle)};
+    private readonly Label m_secondaryLabel = new() {Style = Styles.GetLabelStyle(LabelStyle.Body200), Padding = new Thickness(Sizes.GetSize(SizeName.size_2), 0), BackgroundColor = Colors.GetColor(ColorName.color_fill_subtle)};
 
     private readonly BoxView m_divider = new() { Color = Colors.GetColor(ColorName.color_border_default), Margin = 0};
 
@@ -33,20 +20,15 @@ public partial class Counter : Grid
             new ColumnDefinition {Width = 1},
             new ColumnDefinition {Width = GridLength.Auto}
         ];
-        var primaryLabel = new Label {Style = Styles.GetLabelStyle(LabelStyle.Body200), Margin = new Thickness(Sizes.GetSize(SizeName.size_2), 0)};
-        var secondaryLabel = new Label {Style = Styles.GetLabelStyle(LabelStyle.Body200), Margin = new Thickness(Sizes.GetSize(SizeName.size_2), 0)};
 
         UI.Effects.Layout.Layout.SetCornerRadius(this, Sizes.GetSize(SizeName.size_2));
         
-        m_primaryBorder.Content = primaryLabel;
-        m_secondaryBorder.Content = secondaryLabel;
+        m_primaryLabel.SetBinding(Label.TextProperty, static (Counter counter) => counter.Value, source: this);
+        m_secondaryLabel.SetBinding(Label.TextProperty, static (Counter counter) => counter.SecondaryValue, source: this);
         
-        primaryLabel.SetBinding(Label.TextProperty, static (Counter counter) => counter.Value, source: this);
-        secondaryLabel.SetBinding(Label.TextProperty, static (Counter counter) => counter.SecondaryValue, source: this);
-        
-        this.Add(m_primaryBorder, 0);
+        this.Add(m_primaryLabel, 0);
         this.Add(m_divider, 1);
-        this.Add(m_secondaryBorder, 2);
+        this.Add(m_secondaryLabel, 2);
         
         OnModeChanged();
         OnIsUrgentChanged();
@@ -55,7 +37,7 @@ public partial class Counter : Grid
 
     private void OnIsUrgentChanged()
     {
-        m_primaryBorder.BackgroundColor = Colors.GetColor(IsUrgent ? ColorName.color_fill_danger_subtle : ColorName.color_fill_subtle);
+        m_primaryLabel.BackgroundColor = Colors.GetColor(IsUrgent ? ColorName.color_fill_danger_subtle : ColorName.color_fill_subtle);
         UpdateDivider();
     }
 
@@ -65,20 +47,20 @@ public partial class Counter : Grid
         {
             case CounterDisplayMode.Single:
                 {
-                    m_primaryBorder.IsVisible = true;
-                    m_secondaryBorder.IsVisible = false;
+                    m_primaryLabel.IsVisible = true;
+                    m_secondaryLabel.IsVisible = false;
                     break;
                 }
             case CounterDisplayMode.Double:
                 {
-                    m_primaryBorder.IsVisible = true;
-                    m_secondaryBorder.IsVisible = true;
+                    m_primaryLabel.IsVisible = true;
+                    m_secondaryLabel.IsVisible = true;
                     break;
                 }
             case CounterDisplayMode.Auto:
                 {
-                    m_primaryBorder.IsVisible = true;
-                    m_secondaryBorder.IsVisible = SecondaryValue != 0;
+                    m_primaryLabel.IsVisible = true;
+                    m_secondaryLabel.IsVisible = SecondaryValue != 0;
                     break;
                 }
         }
@@ -88,13 +70,13 @@ public partial class Counter : Grid
 
     private void UpdateDivider()
     {
-        m_divider.IsVisible = m_primaryBorder.IsVisible && m_secondaryBorder.IsVisible;
+        m_divider.IsVisible = m_primaryLabel.IsVisible && m_secondaryLabel.IsVisible;
         m_divider.Color = IsUrgent || IsSecondaryUrgent ? Colors.GetColor(ColorName.color_border_default_hover) : Colors.GetColor(ColorName.color_border_default);
     }
 
     private void OnIsSecondaryUrgentChanged()
     {
-        m_secondaryBorder.BackgroundColor = Colors.GetColor(IsSecondaryUrgent ? ColorName.color_fill_danger_subtle : ColorName.color_fill_subtle);
+        m_secondaryLabel.BackgroundColor = Colors.GetColor(IsSecondaryUrgent ? ColorName.color_fill_danger_subtle : ColorName.color_fill_subtle);
         UpdateDivider();
     }
 
