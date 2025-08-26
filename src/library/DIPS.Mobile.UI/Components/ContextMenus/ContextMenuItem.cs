2e8e8a67c3e3f5873b80890e4ec3590bf54dca94
Vetle444 <vetle_96@live.no>
2025-08-26 15:15:14 +0200

    test this shit yo

--- src/library/DIPS.Mobile.UI/Components/ContextMenus/ContextMenuItem.cs
+++ src/library/DIPS.Mobile.UI/Components/ContextMenus/ContextMenuItem.cs
@@ -5,30 +5,30 @@ namespace DIPS.Mobile.UI.Components.ContextMenus;
 /// </summary>
 public partial class ContextMenuItem : Element, IContextMenuItem
 {
-    internal async void SendClicked(ContextMenu contextMenu)
+    internal void SendClicked(ContextMenu contextMenu)
     {
-
-        
         contextMenu.SendClicked(this);
         Command?.Execute(CommandParameter);
-#if __IOS__
-        // We need a small delay to make sure that something is not presented on top of the context menu's UIViewController, which breaks the app.
-        // This can happen in release, where everything runs fast.
-        await Task.Delay(10);
-#endif
+
         DidClick?.Invoke(this, EventArgs.Empty);
         
         var contextMenuLoggingMetadata = new GlobalContextMenuClickMetadata(this, contextMenu);
         var didSendGlobalClick = false;
         if (contextMenu.ItemsShouldSendGlobalClicks) //If consumer wants to send clicks globally for all items
         {
-            ContextMenuEffect.ContextMenuItemGlobalClicksCallBack?.Invoke(contextMenuLoggingMetadata);
+            Task.Run(() =>
+            {
+                ContextMenuEffect.ContextMenuItemGlobalClicksCallBack?.Invoke(contextMenuLoggingMetadata);
+            });
             didSendGlobalClick = true;
         }
         
         if (ShouldSendGlobalClick && !didSendGlobalClick) //If consumer wants a single item to log, no matter what parent says
         {
-            ContextMenuEffect.ContextMenuItemGlobalClicksCallBack?.Invoke(contextMenuLoggingMetadata);    
+            Task.Run(() =>
+            {
+                ContextMenuEffect.ContextMenuItemGlobalClicksCallBack?.Invoke(contextMenuLoggingMetadata);
+            });
         }
     }
 
