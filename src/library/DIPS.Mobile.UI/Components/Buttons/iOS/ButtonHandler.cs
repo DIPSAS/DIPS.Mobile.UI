350fdada694883e632459f72808f3bb06b5ba82c
Vetle444 <vetle_96@live.no>
2025-08-27 19:35:33 +0200

    done

--- src/library/DIPS.Mobile.UI/Components/Buttons/iOS/ButtonHandler.cs
+++ src/library/DIPS.Mobile.UI/Components/Buttons/iOS/ButtonHandler.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using CoreGraphics;
 using DIPS.Mobile.UI.Platforms.iOS;
 using Microsoft.Maui.Platform;
@@ -11,6 +12,26 @@ public partial class ButtonHandler : Microsoft.Maui.Handlers.ButtonHandler
     {
         var button = new UIButtonWithExtraTappableArea();
         return button;
+    }
+
+    protected override void ConnectHandler(UIButton platformView)
+    {
+        base.ConnectHandler(platformView);
+        
+        (VirtualView as View).PropertyChanged += OnPropertyChanged;
+    }
+
+    /// <summary>
+    /// TODO: Remove when this PR is merged: https://github.com/dotnet/maui/pull/25107
+    /// <remarks>When Button's IsVisible is toggled the Image is reset by .NET MAUI so we need to set the tint color again, but it happens after this method is called, hence the Task.Delay</remarks>
+    /// </summary>
+    private async void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == View.IsVisibleProperty.PropertyName)
+        {
+            await Task.Delay(1);
+            MapImageTintColor(this, VirtualView as Button);
+        }
     }
 
     private partial void AppendPropertyMapper()
@@ -40,13 +61,20 @@ public partial class ButtonHandler : Microsoft.Maui.Handlers.ButtonHandler
         if(handler.PlatformView.ImageView.Image is null)
             return;
         
-        handler.PlatformView.SetImage(handler.PlatformView.ImageView.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate), UIControlState.Normal);
         handler.PlatformView.ImageView.TintColor = button.ImageTintColor.ToPlatform();
+        handler.PlatformView.SetImage(handler.PlatformView.ImageView.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate), UIControlState.Normal);
     }
 
     private static partial void MapAdditionalHitBoxSize(ButtonHandler handler, Button button)
     {
         if (handler.PlatformView is UIButtonWithExtraTappableArea uiButton)
             uiButton.AdditionalHitBoxSize = button.AdditionalHitBoxSize;
+    }
+
+    protected override void DisconnectHandler(UIButton platformView)
+    {
+        base.DisconnectHandler(platformView);
+        
+        (VirtualView as View).PropertyChanged += OnPropertyChanged;
     }
 }
