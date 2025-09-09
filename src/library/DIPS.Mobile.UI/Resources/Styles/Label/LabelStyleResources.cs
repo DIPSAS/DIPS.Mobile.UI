dd140d2302a4a70f425acb404578fc2420e5ecc2
Vetle444 <vetle_96@live.no>
2025-09-09 13:44:39 +0200

    try getters

--- src/library/DIPS.Mobile.UI/Resources/Styles/Label/LabelStyleResources.cs
+++ src/library/DIPS.Mobile.UI/Resources/Styles/Label/LabelStyleResources.cs
@@ -4,29 +4,30 @@ namespace DIPS.Mobile.UI.Resources.Styles.Label;
 
 internal class LabelStyleResources
 {
-    public static readonly Dictionary<LabelStyle, Style> Styles;
-    
-    static LabelStyleResources()
+    public static Dictionary<LabelStyle, Style> Styles
     {
-        var sharedStyles = SharedStyleResources.GetStylesForType(typeof(Components.Labels.Label));
-        Styles = new Dictionary<LabelStyle, Style>
+        get
         {
-            [LabelStyle.None] = sharedStyles[TextStyle.None],
-            [LabelStyle.Body400] = sharedStyles[TextStyle.Body400],
-            [LabelStyle.Body300] = sharedStyles[TextStyle.Body300],
-            [LabelStyle.Body200] = sharedStyles[TextStyle.Body200],
-            [LabelStyle.Body100] = sharedStyles[TextStyle.Body100],
-            [LabelStyle.UI400] = sharedStyles[TextStyle.UI400],
-            [LabelStyle.UI300] = sharedStyles[TextStyle.UI300],
-            [LabelStyle.UI200] = sharedStyles[TextStyle.UI200],
-            [LabelStyle.UI100] = sharedStyles[TextStyle.UI100],
-            [LabelStyle.Header1000] = sharedStyles[TextStyle.Header1000],
-            [LabelStyle.Header900] = sharedStyles[TextStyle.Header900],
-            [LabelStyle.Header800] = sharedStyles[TextStyle.Header800],
-            [LabelStyle.Header700] = sharedStyles[TextStyle.Header700],
-            [LabelStyle.Header600] = sharedStyles[TextStyle.Header600],
-            [LabelStyle.Header500] = sharedStyles[TextStyle.Header500],
-            [LabelStyle.SectionHeader] = sharedStyles[TextStyle.SectionHeader]
-        };
+            var sharedStyles = SharedStyleResources.GetStylesForType(typeof(Components.Labels.Label));
+            return new Dictionary<LabelStyle, Style>
+            {
+                [LabelStyle.None] = sharedStyles[TextStyle.None],
+                [LabelStyle.Body400] = sharedStyles[TextStyle.Body400],
+                [LabelStyle.Body300] = sharedStyles[TextStyle.Body300],
+                [LabelStyle.Body200] = sharedStyles[TextStyle.Body200],
+                [LabelStyle.Body100] = sharedStyles[TextStyle.Body100],
+                [LabelStyle.UI400] = sharedStyles[TextStyle.UI400],
+                [LabelStyle.UI300] = sharedStyles[TextStyle.UI300],
+                [LabelStyle.UI200] = sharedStyles[TextStyle.UI200],
+                [LabelStyle.UI100] = sharedStyles[TextStyle.UI100],
+                [LabelStyle.Header1000] = sharedStyles[TextStyle.Header1000],
+                [LabelStyle.Header900] = sharedStyles[TextStyle.Header900],
+                [LabelStyle.Header800] = sharedStyles[TextStyle.Header800],
+                [LabelStyle.Header700] = sharedStyles[TextStyle.Header700],
+                [LabelStyle.Header600] = sharedStyles[TextStyle.Header600],
+                [LabelStyle.Header500] = sharedStyles[TextStyle.Header500],
+                [LabelStyle.SectionHeader] = sharedStyles[TextStyle.SectionHeader]
+            };
+        }
     }
 }
--- src/library/DIPS.Mobile.UI/Resources/Styles/Span/SpanStyleResources.cs
+++ src/library/DIPS.Mobile.UI/Resources/Styles/Span/SpanStyleResources.cs
@@ -4,29 +4,30 @@ namespace DIPS.Mobile.UI.Resources.Styles.Span;
 
 internal class SpanStyleResources
 {
-    public static readonly Dictionary<SpanStyle, Style> Styles;
-    
-    static SpanStyleResources()
+    public static Dictionary<SpanStyle, Style> Styles
     {
-        var sharedStyles = SharedStyleResources.GetStylesForType(typeof(Microsoft.Maui.Controls.Span));
-        Styles = new Dictionary<SpanStyle, Style>
+        get
         {
-            [SpanStyle.None] = sharedStyles[TextStyle.None],
-            [SpanStyle.Body400] = sharedStyles[TextStyle.Body400],
-            [SpanStyle.Body300] = sharedStyles[TextStyle.Body300],
-            [SpanStyle.Body200] = sharedStyles[TextStyle.Body200],
-            [SpanStyle.Body100] = sharedStyles[TextStyle.Body100],
-            [SpanStyle.UI400] = sharedStyles[TextStyle.UI400],
-            [SpanStyle.UI300] = sharedStyles[TextStyle.UI300],
-            [SpanStyle.UI200] = sharedStyles[TextStyle.UI200],
-            [SpanStyle.UI100] = sharedStyles[TextStyle.UI100],
-            [SpanStyle.Header1000] = sharedStyles[TextStyle.Header1000],
-            [SpanStyle.Header900] = sharedStyles[TextStyle.Header900],
-            [SpanStyle.Header800] = sharedStyles[TextStyle.Header800],
-            [SpanStyle.Header700] = sharedStyles[TextStyle.Header700],
-            [SpanStyle.Header600] = sharedStyles[TextStyle.Header600],
-            [SpanStyle.Header500] = sharedStyles[TextStyle.Header500],
-            [SpanStyle.SectionHeader] = sharedStyles[TextStyle.SectionHeader]
-        };
+            var sharedStyles = SharedStyleResources.GetStylesForType(typeof(Microsoft.Maui.Controls.Span));
+            return new Dictionary<SpanStyle, Style>
+            {
+                [SpanStyle.None] = sharedStyles[TextStyle.None],
+                [SpanStyle.Body400] = sharedStyles[TextStyle.Body400],
+                [SpanStyle.Body300] = sharedStyles[TextStyle.Body300],
+                [SpanStyle.Body200] = sharedStyles[TextStyle.Body200],
+                [SpanStyle.Body100] = sharedStyles[TextStyle.Body100],
+                [SpanStyle.UI400] = sharedStyles[TextStyle.UI400],
+                [SpanStyle.UI300] = sharedStyles[TextStyle.UI300],
+                [SpanStyle.UI200] = sharedStyles[TextStyle.UI200],
+                [SpanStyle.UI100] = sharedStyles[TextStyle.UI100],
+                [SpanStyle.Header1000] = sharedStyles[TextStyle.Header1000],
+                [SpanStyle.Header900] = sharedStyles[TextStyle.Header900],
+                [SpanStyle.Header800] = sharedStyles[TextStyle.Header800],
+                [SpanStyle.Header700] = sharedStyles[TextStyle.Header700],
+                [SpanStyle.Header600] = sharedStyles[TextStyle.Header600],
+                [SpanStyle.Header500] = sharedStyles[TextStyle.Header500],
+                [SpanStyle.SectionHeader] = sharedStyles[TextStyle.SectionHeader]
+            };
+        }
     }
 }
