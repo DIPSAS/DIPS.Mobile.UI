e3b21273159fd0724b8e4ba5233eaef4f64ef209
Håvard Moås <hmo@dips.no>
2025-10-15 09:19:53 +0200

    Less build errors on windows (#726)

--- build/bootstrapper/bootstrapper.sh
+++ build/bootstrapper/bootstrapper.sh
@@ -26,24 +26,29 @@ CheckIfInstalledCorrectly ()
 }
 
 echo "🥾 ---- Running bootstrapper ---- 🥾"
-# Check if you are running macos
-if sw_vers -productname | grep -q 'macOS' ; then
-   echo "✅ You are running on Apple software."
-   echo "Trying to select Xcode 16.1"
-   sudo xcode-select -s /Applications/Xcode_16.1.app #This is mainly used to force Azure to use a specific Xcode version, Xcode Azure paths are found here: https://github.com/actions/runner-images/blob/macos-14/20240923.101/images/macos/macos-14-Readme.md#xcode
-   echo "✅ You are now running on Xcode 16.1"
-else
-   echo "❌ You are not running on  software. This build system requires you to run on a Mac."
-fi
 
-#dotnet-script
+# -- x-platform stuff --
 
+# dotnet-script
 if dotnet tool list -g | grep dotnet-script > /dev/null ; then
    echo "✅ dotnet-script was found"
 else
    echo "❌ dotnet-script was not found, installing..."
    dotnet tool install -g dotnet-script > /dev/null
    CheckIfInstalledCorrectly "dotnet-script"
+fi
+
+# -- macOS stuff --
+
+# Check if you are running macos
+if [[ "$(uname -s 2>/dev/null)" == "Darwin" ]]; then
+   echo "✅ You are running on Apple software."
+   echo "Trying to select Xcode 16.1"
+   sudo xcode-select -s /Applications/Xcode_16.1.app #This is mainly used to force Azure to use a specific Xcode version, Xcode Azure paths are found here: https://github.com/actions/runner-images/blob/macos-14/20240923.101/images/macos/macos-14-Readme.md#xcode
+   echo "✅ You are now running on Xcode 16.1"
+else
+   echo "❌ You are not running on  software. This build system requires you to run on a Mac."
+   exit 0
 fi
 
 
