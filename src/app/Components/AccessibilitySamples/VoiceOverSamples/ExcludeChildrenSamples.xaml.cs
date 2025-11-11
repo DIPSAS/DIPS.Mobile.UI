b50fb068a2b556d47dfdf88b7dd0bfb60f8cffd0
Vetle444 <vetle_96@live.no>
2025-11-11 15:05:23 +0100

    remove

--- src/app/Components/AccessibilitySamples/VoiceOverSamples/ExcludeChildrenSamples.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Components.AccessibilitySamples.VoiceOverSamples;
-
-public partial class ExcludeChildrenSamples : DIPS.Mobile.UI.Components.Pages.ContentPage
-{
-    public ExcludeChildrenSamples()
-    {
-        InitializeComponent();
-    }
-}
