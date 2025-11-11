3991ed2271b34e422721e689f54ceb72c1edfb94
Vetle444 <vetle_96@live.no>
2025-11-11 15:58:03 +0100

    fix error

--- src/app/Components/AccessibilitySamples/VoiceOverSamples/VoiceOverSamples.xaml.cs
+++ src/app/Components/AccessibilitySamples/VoiceOverSamples/VoiceOverSamples.xaml.cs
@@ -1,27 +1,9 @@
-using System.Windows.Input;
-
 namespace Components.AccessibilitySamples.VoiceOverSamples;
 
-public partial class VoiceOverSamples : DIPS.Mobile.UI.Components.Pages.ContentPage
+public partial class VoiceOverSamples
 {
     public VoiceOverSamples()
     {
         InitializeComponent();
-        
-        NavigateToGroupChildrenCommand = new Command(NavigateToGroupChildren);
-        NavigateToExcludeChildrenCommand = new Command(NavigateToExcludeChildren);
-    }
-
-    public ICommand NavigateToGroupChildrenCommand { get; }
-    public ICommand NavigateToExcludeChildrenCommand { get; }
-
-    private void NavigateToGroupChildren()
-    {
-        Shell.Current.Navigation.PushAsync(new GroupChildrenSamples());
-    }
-
-    private void NavigateToExcludeChildren()
-    {
-        Shell.Current.Navigation.PushAsync(new ExcludeChildrenSamples());
     }
 }
