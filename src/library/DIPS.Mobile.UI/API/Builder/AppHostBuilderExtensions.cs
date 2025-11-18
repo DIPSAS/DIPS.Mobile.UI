e061e98f9a61d0f8116c63bc4e13426d49252186
Vetle444 <vetle_96@live.no>
2025-11-18 09:00:42 +0100

    good progress

--- src/library/DIPS.Mobile.UI/API/Builder/AppHostBuilderExtensions.cs
+++ src/library/DIPS.Mobile.UI/API/Builder/AppHostBuilderExtensions.cs
@@ -10,6 +10,7 @@ using DIPS.Mobile.UI.Components.Navigation.FloatingNavigationButton;
 using DIPS.Mobile.UI.Components.Pickers.ScrollPicker;
 using DIPS.Mobile.UI.Effects.Animation.Effects;
 using DIPS.Mobile.UI.Effects.Touch;
+using DotNet.Meteor.HotReload.Plugin;
 using Microsoft.Maui.LifecycleEvents;
 using SkiaSharp.Views.Maui.Controls.Hosting;
 using ButtonHandler = DIPS.Mobile.UI.Components.Buttons.ButtonHandler;
@@ -44,6 +45,10 @@ public static partial class AppHostBuilderExtensions
     {
         //Initializers
         DUI.Init();
+        
+#if DEBUG
+        builder.EnableHotReload();
+#endif
         
         builder.ConfigureLifecycleEvents(ConfigurePlatformLifecycleEvents);
         
--- src/library/DIPS.Mobile.UI/AssemblyInfo.cs
+++ src/library/DIPS.Mobile.UI/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Runtime.CompilerServices;
 using Microsoft.Maui.Controls.Internals;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+/*[assembly: XamlCompilation(XamlCompilationOptions.Compile)]*/
 [assembly: InternalsVisibleTo("DIPS.Mobile.UI.UnitTests")]
 [assembly: InternalsVisibleTo("Components")]
 
