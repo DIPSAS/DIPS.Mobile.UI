38133e160859c6bff972276144c86e453ddf6038
Vetle444 <vetle_96@live.no>
2026-01-14 14:20:25 +0100

    use xcode 26.2

--- build/bootstrapper/bootstrapper.sh
+++ build/bootstrapper/bootstrapper.sh
@@ -43,9 +43,9 @@ fi
 # Check if you are running macos
 if [[ "$(uname -s 2>/dev/null)" == "Darwin" ]]; then
    echo "✅ You are running on Apple software."
-   echo "Trying to select Xcode 26.1"
-   sudo xcode-select -s /Applications/Xcode_26.1.1.app #This is mainly used to force Azure to use a specific Xcode version, Xcode Azure paths are found here: https://github.com/actions/runner-images/blob/macos-14/20240923.101/images/macos/macos-14-Readme.md#xcode
-   echo "✅ You are now running on Xcode 26.1"
+   echo "Trying to select Xcode 26.2"
+   sudo xcode-select -s /Applications/Xcode_26.2.app #This is mainly used to force Azure to use a specific Xcode version, Xcode Azure paths are found here: https://github.com/actions/runner-images/blob/macos-14/20240923.101/images/macos/macos-14-Readme.md#xcode
+   echo "✅ You are now running on Xcode 26.2"
 else
    echo "❌ You are not running on  software. This build system requires you to run on a Mac."
    exit 0
