0f456807715387da313e39bdd0c00565fdc6fa8e
Vetle Finstad <finstad@Vetles-MacBook-Pro-2.local>
2026-05-28 14:29:57 +0200

    Fix initial time clamping and async scroll-back for TimePicker constraints

--- src/library/DIPS.Mobile.UI/Components/Pickers/TimePicker/iOS/TimePickerHandler.cs
+++ src/library/DIPS.Mobile.UI/Components/Pickers/TimePicker/iOS/TimePickerHandler.cs
@@ -8,7 +8,7 @@ namespace DIPS.Mobile.UI.Components.Pickers.TimePicker;
 public partial class TimePickerHandler : BaseDatePickerHandler
 {
     private bool m_isClampingTime;
-    
+
     protected override UIDatePicker CreatePlatformView()
     {
         return new UIDatePicker
@@ -22,7 +22,7 @@ public partial class TimePickerHandler : BaseDatePickerHandler
     {
         if (m_isClampingTime)
             return;
-        
+
         if (VirtualView is not TimePicker timePicker)
             return;
 
@@ -47,25 +47,50 @@ public partial class TimePickerHandler : BaseDatePickerHandler
     private void ClampPickerTime(TimeSpan time)
     {
         m_isClampingTime = true;
-        
+
         var calendar = NSCalendar.CurrentCalendar;
         var components = calendar.Components(NSCalendarUnit.Year | NSCalendarUnit.Month | NSCalendarUnit.Day | NSCalendarUnit.Hour | NSCalendarUnit.Minute, PlatformView.Date);
         components.Hour = time.Hours;
         components.Minute = time.Minutes;
-        PlatformView.SetDate(calendar.DateFromComponents(components), true);
-        
-        m_isClampingTime = false;
-        
+        var clampedDate = calendar.DateFromComponents(components);
+
+        // Dispatch to let the picker's internal animation settle before scrolling back
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            PlatformView.SetDate(clampedDate, true);
+            m_isClampingTime = false;
+        });
+
         VirtualView.SetSelectedDateTime(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, time.Hours, time.Minutes, 0));
+    }
+
+    private static void SetPickerDate(TimePickerHandler handler, TimeSpan time)
+    {
+        var calendar = NSCalendar.CurrentCalendar;
+        var components = calendar.Components(
+            NSCalendarUnit.Year | NSCalendarUnit.Month | NSCalendarUnit.Day | NSCalendarUnit.Hour | NSCalendarUnit.Minute,
+            handler.PlatformView.Date);
+        components.Hour = time.Hours;
+        components.Minute = time.Minutes;
+        handler.PlatformView.SetDate(calendar.DateFromComponents(components), false);
+    }
+
+    private static TimeSpan ClampTime(TimePicker timePicker, TimeSpan time)
+    {
+        if (timePicker.MinimumTime is { } min && time < min)
+            return min;
+        if (timePicker.MaximumTime is { } max && time > max)
+            return max;
+        return time;
     }
 
     private static partial void MapSelectedTime(TimePickerHandler handler, TimePicker timePicker)
     {
-        var calendar = NSCalendar.CurrentCalendar;
-        var components = NSCalendar.CurrentCalendar.Components(NSCalendarUnit.Hour | NSCalendarUnit.Minute, new NSDate());
-        components.Hour = timePicker.SelectedTime.Hours;
-        components.Minute = timePicker.SelectedTime.Minutes;
-        handler.PlatformView.SetDate(calendar.DateFromComponents(components), true);
+        var clamped = ClampTime(timePicker, timePicker.SelectedTime);
+        if (clamped != timePicker.SelectedTime)
+            timePicker.SelectedTime = clamped;
+        
+        SetPickerDate(handler, clamped);
     }
 
     private static partial void MapMinimumTime(TimePickerHandler handler, TimePicker timePicker)
@@ -83,6 +108,8 @@ public partial class TimePickerHandler : BaseDatePickerHandler
         components.Hour = minimumTime.Hours;
         components.Minute = minimumTime.Minutes;
         handler.PlatformView.MinimumDate = calendar.DateFromComponents(components);
+        
+        MapSelectedTime(handler, timePicker);
     }
 
     private static partial void MapMaximumTime(TimePickerHandler handler, TimePicker timePicker)
@@ -100,6 +127,8 @@ public partial class TimePickerHandler : BaseDatePickerHandler
         components.Hour = maximumTime.Hours;
         components.Minute = maximumTime.Minutes;
         handler.PlatformView.MaximumDate = calendar.DateFromComponents(components);
+        
+        MapSelectedTime(handler, timePicker);
     }
 
 
